b7b40e517dc15d6119cbb594cb813dd18b968fce
Timothy Clemans <timothy.clemans@gmail.com>
2012-09-07 13:58:12 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+try:
+    README = open('README').read()
+except:
+    README = None
+
+setup(
+    name='django-admin-sortable',
+    version=__import__('adminsortable').__version__,
+    description='Drag and drop sorting for models and inline models in Django admin',
+    long_description=README,
+    license='APL',
+    author='Timothy Clemans',
+    author_email='timothyclemans@gmail.com',
+    packages=find_packages(exclude=['sample_project']),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=['Development Status :: 5 - Production/Stable',
+                   'Environment :: Web Environment',
+                   'Framework :: Django',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+)
